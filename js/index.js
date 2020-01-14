4cfc98cc3b227df5a07d14b9ed6aad1513f96a28
BigBankElit3 <57700160+n0m0n3y@users.noreply.github.com>
2020-01-14 17:48:27 -0500

    keydown! Thanks dustin

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 // Your code goes here
+const gone = document.querySelector("img");
+
+window.addEventListener("resize", () => {//8
+  gone.src = "/img/gone.jpg";
+});
+console.log(gone);
 
 //soundFiles
 const spart = document.querySelector('body');
-spart.addEventListener('mouseenter',()=>{
+spart.addEventListener('mouseenter',()=>{//7
     anons.play();
 
     
@@ -23,51 +29,58 @@ bleep.src = '/img/fake-news.mp3';
 
 
 const newNav = document.querySelector('.nav');
-newNav.addEventListener('click', (event)=>{
-    alert ('These buttons will take you ANY WHERE! You did not take heed I see, welcome to the DarkSide!');
-event.preventDefault(event);
+newNav.addEventListener('click', (event)=>{//6
+    alert ('These buttons will not take you ANY WHERE! You did not take heed I see, welcome to the DarkSide!');
+event.preventDefault();
 });
 
 const funBus = document.querySelector('.funBusImg');
-funBus.addEventListener('mouseover', ()=>{
+funBus.addEventListener('mouseleave', ()=>{//dub
     
     alert ('RUNNNNNNNN!!!!!! WHILE YOU CAN THIS SITE IS POISONED!');
 });
 
-const funBusH =document.querySelector('h2');
-funBusH.addEventListener('select',
-    ()=>{
-h2.style.color ="red";
-    });
-console.log(funBusH);
 
 
+ 
 
 
 const clickFor = document.querySelector('.btn');
-clickFor.addEventListener('click',(event)=>{
+window.addEventListener('keypress',(event)=>{//5
  bleep.play();
   event.stopPropagation();
     
 });
 
 const btnOne = document.querySelector('.one');
-btnOne.addEventListener('dblclick', (event)=>{
+btnOne.addEventListener('dblclick', (event)=>{//4
     alert ('OK OK OK, Last Chance. Just Leave Now, Before Things Get DARK. ');
    
 });
 
 const btnTwo = document.querySelector('.two');
-btnTwo.addEventListener('mouseleave', (event)=>{
+btnTwo.addEventListener('mouseleave', (event)=>{//3
     mario.play();
    
 });
 
 const btnThree = document.querySelector('.three');
-btnThree.addEventListener('mouseover', (event)=>{
+btnThree.addEventListener('mouseover', (event)=>{//2
     anObama.play();
     
 
 
 });
 
+const foot = document.querySelector('footer');
+window.addEventListener('keydown', //1
+    ()=>{
+foot.style.backgroundColor="red";
+    });
+console.log(foot);
+
+const vol = document.querySelector('body');
+window.addEventListener('volumechange',(event)=>{
+alert('DONT TOUCH THE VOL!');
+console.log(vol);
+});
