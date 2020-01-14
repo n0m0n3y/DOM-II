2e56aa4f6eea18753b421faa82364478f89b6781
BigBankElit3 <57700160+n0m0n3y@users.noreply.github.com>
2020-01-14 18:09:30 -0500

    MVP

--- js/index.js
+++ js/index.js
@@ -1,20 +1,24 @@
 // Your code goes here
+const pointer = document.querySelector('.img-fluid');
+pointer.addEventListener('pointerenter',(event)=>{//10!
+alert('DONT TOUCH THAT IMAGE!');
+console.log(pointer);
+});
 const gone = document.querySelector("img");
 
-window.addEventListener("resize", () => {//8
+window.addEventListener("resize", () => {//9
   gone.src = "/img/gone.jpg";
 });
 console.log(gone);
 
 //soundFiles
 const spart = document.querySelector('body');
-spart.addEventListener('mouseenter',()=>{//7
+spart.addEventListener('mouseenter',()=>{//8
     anons.play();
 
     
 });
 const obama = document.querySelector('body');
-
 obama.addEventListener('',()=>{
     obama.play();
 });
@@ -29,13 +33,13 @@ bleep.src = '/img/fake-news.mp3';
 
 
 const newNav = document.querySelector('.nav');
-newNav.addEventListener('click', (event)=>{//6
+newNav.addEventListener('click', (event)=>{//7
     alert ('These buttons will not take you ANY WHERE! You did not take heed I see, welcome to the DarkSide!');
 event.preventDefault();
 });
 
 const funBus = document.querySelector('.funBusImg');
-funBus.addEventListener('mouseleave', ()=>{//dub
+funBus.addEventListener('mousemove', ()=>{//6
     
     alert ('RUNNNNNNNN!!!!!! WHILE YOU CAN THIS SITE IS POISONED!');
 });
@@ -46,7 +50,7 @@ funBus.addEventListener('mouseleave', ()=>{//dub
 
 
 const clickFor = document.querySelector('.btn');
-window.addEventListener('keypress',(event)=>{//5
+clickFor.addEventListener('pointerdown',(event)=>{//5
  bleep.play();
   event.stopPropagation();
     
@@ -79,8 +83,4 @@ foot.style.backgroundColor="red";
     });
 console.log(foot);
 
-const vol = document.querySelector('body');
-window.addEventListener('volumechange',(event)=>{
-alert('DONT TOUCH THE VOL!');
-console.log(vol);
-});
+
